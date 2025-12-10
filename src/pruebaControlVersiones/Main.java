82a9a943164adb212068205eb9559f55925b85a4
scassanc <scassanc@g.educaand.es>
2025-12-10 08:43:12 +0100

    Versión F3: añadir JavaDoc al método del factorial

--- src/pruebaControlVersiones/Main.java
+++ src/pruebaControlVersiones/Main.java
@@ -28,6 +28,12 @@ public class Main {
     return result;
   }
 
+  /**
+   * Calcula el factorial de un número utilizando un enfoque recursivo.
+   * 
+   * @param n Número entero del que se quiere obtener el factorial.
+   * @return Factorial de n, calculado mediante recursividad.
+   */
   private static long factorialRec(int n) {
     if (n == 0 || n == 1) {
       return 1;
