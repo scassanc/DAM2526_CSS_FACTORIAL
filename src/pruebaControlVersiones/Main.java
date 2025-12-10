f0ad674fbf917eb71573ec82ee90a9098f562f03
scassanc <scassanc@g.educaand.es>
2025-12-10 08:34:40 +0100

    Versión B: refactorizar código base para solicitar un número al usuario

--- src/pruebaControlVersiones/Main.java
+++ src/pruebaControlVersiones/Main.java
@@ -3,16 +3,20 @@ package pruebaControlVersiones;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  private static Scanner sc = new Scanner(System.in);
 
-    System.out.print("Introduce un número entero mayor o igual que 0: ");
-    int num = sc.nextInt();
+  public static void main(String[] args) {
+    int num = pedirNumero();
 
     if (num < 0) {
       System.out.println("ERROR: el número debe ser mayor o igual que 0.");
     } else {
       System.out.println("Has introducido el número: " + num);
     }
+  }
+
+  private static int pedirNumero() {
+    System.out.print("Introduce un número entero mayor o igual que 0: ");
+    return sc.nextInt();
   }
 }
